0459d6f7ff2fa6355ab087d15bf4bf0e09381859
David Strauss <david@davidstrauss.net>
2012-04-25 21:05:07 +0000

    Allow import.

--- perfbucket/export_for_xhprof.py
+++ perfbucket/export_for_xhprof.py
@@ -7,4 +7,5 @@ def get_xhprof_data(request_uuid):
     details = storage.get_profiling_details(request_uuid)
     return phpserialize.dumps(details["data"])
 
-print(get_xhprof_data(uuid.UUID(sys.argv[1])))
+if __name__ == '__main__':
+    print(get_xhprof_data(uuid.UUID(sys.argv[1])))
