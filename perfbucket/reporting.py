eaee8c32523ca9751c9e828d2dd2d2dc20b25b39
David Strauss <david@davidstrauss.net>
2012-04-26 17:00:45 +0000

    Only show hours with results. Only show requests in verbose mode.

--- perfbucket/reporting.py
+++ perfbucket/reporting.py
@@ -9,26 +9,30 @@ def print_hour(timestamp):
 
 # All paths slower than "N" (5000 milliseconds)
 
-def slowest_requests_by_hour(begin=None, hours=24):
+def slowest_requests_by_hour(begin=None, hours=24, verbose=False):
     print("Slowest Requests by Hour")
 
     if begin is None:
         begin = time.time()
     for hours_ago in range(0, hours):
         timestamp = begin - hours_ago * 60 * 60
-        print_hour(timestamp)
         results = storage.get_worst(timestamp)
-        for name, value in results.iteritems():
-            request_uuid = value["request_uuid"]
-            print("  Request UUID: %s" % request_uuid)
-            print("    Duration: {0} milliseconds".format(value["duration"] / 1000))
-            print("    Page: {0}".format(value["page"]))
+        if len(results) > 0:
+            print_hour(timestamp)
+            for name, value in results.iteritems():
+                request_uuid = value["request_uuid"]
+                print("  Request UUID: %s" % request_uuid)
+                print("    Duration: {0} milliseconds".format(value["duration"] / 1000))
+                print("    Page: {0}".format(value["page"]))
 
 # Top 20 slowest by average wall time (weighted by popularity?)
 
 def _slowest_pages_on_average_for_hour(timestamp, top, min_count):
     pages = {}
     results = storage.get_worst(timestamp, max_count=10000)
+    if len(results) == 0:
+        return
+    print_hour(timestamp)
     for name, value in results.iteritems():
         current = pages.get(value["page"], {"count": 0, "duration": 0, "requests": {}})
         current["requests"][value["duration"]] = str(value["request_uuid"])
@@ -48,26 +52,26 @@ def _slowest_pages_on_average_for_hour(timestamp, top, min_count):
         print("  Page: {0}".format(page))
         print("    Count: {0}".format(value["count"]))
         print("    Average duration: {0} milliseconds".format(average_duration / 1000))
-        print("    Worst requests (showing up to ten):")
+        if verbose:
+            print("    Worst requests (showing up to ten):")
 
-        requests_displayed = 0
-        for duration in reversed(sorted(value["requests"].keys())):
-            print("      {0}".format(value["requests"][duration]))
-            requests_displayed += 1
-            if requests_displayed == 10:
+            requests_displayed = 0
+            for duration in reversed(sorted(value["requests"].keys())):
+                print("      {0}".format(value["requests"][duration]))
+                requests_displayed += 1
+                if requests_displayed == 10:
+                    break
+
+            displayed += 1
+            if displayed == top:
                 break
 
-        displayed += 1
-        if displayed == top:
-            break
-
-def slowest_pages_on_average_by_hour(begin=None, hours=24, top=50, min_count=2):
+def slowest_pages_on_average_by_hour(begin=None, hours=24, top=50, min_count=2, verbose=False):
     print("Slowest Pages on Average by Hour")
     if begin is None:
         begin = time.time()
     for hours_ago in range(0, hours):
         timestamp = begin - hours_ago * 60 * 60
-        print_hour(timestamp)
         _slowest_pages_on_average_for_hour(timestamp, top, min_count)
 
 if __name__ == '__main__':
